cd192c5ae31ee047630c1c4a1dd5744d7f77eac9
Hyuchiha <kev.pachec.lis@gmail.com>
2020-06-13 18:36:53 -0500

    Added support for v1_13_R1

---
+++ v1_13_R1/src/main/java/com/hyuchiha/village_defense/Mobs/v1_13_R1/CustomEntityType.java
@@ -0,0 +1,54 @@
+package com.hyuchiha.village_defense.Mobs.v1_13_R1;
+
+import com.hyuchiha.village_defense.Mobs.EntityType;
+import com.hyuchiha.village_defense.Mobs.v1_13_R1.NMS.*;
+import net.minecraft.server.v1_13_R1.Entity;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_13_R1.CraftWorld;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+public enum CustomEntityType implements EntityType {
+  CUSTOM_ZOMBIE("Zombie", 54, CustomZombie.class),
+  CUSTOM_WITCH("Witch", 66, CustomWitch.class),
+  CUSTOM_SPIDER("Spider", 52, CustomSpider.class),
+  CUSTOM_SKELETON("Skeleton", 51, CustomSkeleton.class),
+  CUSTOM_WITHER_SKELETON("Wither_Skeleton", 5, CustomWitherSkeleton.class),
+  CUSTOM_PIGZOMBIE("PigZombie", 57, CustomPigZombie.class),
+  CUSTOM_IRONGOLEM("VillagerGolem", 99, CustomIronGolem.class),
+  CUSTOM_CREEPER("Creeper", 50, CustomCreeper.class),
+  CUSTOM_CAVESPIDER("CaveSpider", 59, CustomCaveSpider.class),
+  CUSTOM_MAGMA_CUBE("MagmaCube", 62, CustomMagmaCube.class),
+  CUSTOM_SLIME("Slime", 55, CustomSlime.class),
+  CUSTOM_GIANT("Giant", 53, CustomGiant.class),
+  CUSTOM_WITHER("Wither", 64, CustomWither.class);
+
+  CustomEntityType(String name, int id, Class<? extends Entity> custom) {
+    addToMaps(custom, name, id);
+  }
+
+  public org.bukkit.entity.Entity spawnEntity(Location location) {
+    Entity entity = getEntity(this, location.getWorld());
+
+    entity.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+    ((CraftWorld) location.getWorld()).getHandle().addEntity(entity, CreatureSpawnEvent.SpawnReason.CUSTOM);
+
+    return entity.getBukkitEntity();
+  }
+
+  public void addToMaps(Class clazz, String name, int id) {
+    CustomEntityRegistry.registerCustomEntity(id, name, clazz);
+  }
+
+  public Entity getEntity(CustomEntityType entity, World world) {
+    CraftWorld craftWorld = (CraftWorld) world;
+
+    switch (entity) {
+      case CUSTOM_WITHER:
+        return new CustomWither(craftWorld.getHandle());
+      case CUSTOM_ZOMBIE:
+      default:
+        return new CustomZombie(craftWorld.getHandle());
+    }
+  }
+}
---
+++ v1_13_R1/src/main/java/com/hyuchiha/village_defense/Mobs/v1_13_R1/NMS/CustomSpider.java
@@ -0,0 +1,36 @@
+package com.hyuchiha.village_defense.Mobs.v1_13_R1.NMS;
+
+import com.hyuchiha.village_defense.Mobs.MobUtils;
+import net.minecraft.server.v1_13_R1.*;
+
+import java.util.Set;
+
+/**
+ * @author hyuchiha
+ */
+public class CustomSpider extends EntitySpider {
+
+  public CustomSpider(World world) {
+    super(world);
+    Set goalB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    goalB.clear();
+    Set goalC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    goalC.clear();
+    Set targetB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    targetB.clear();
+    Set targetC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    targetC.clear();
+
+    this.goalSelector.a(0, new PathfinderGoalFloat(this));
+    this.goalSelector.a(2, new PathfinderGoalMeleeAttack(this, 1.0D, false));
+    this.goalSelector.a(4, new PathfinderGoalMeleeAttack(this, 1.0D, true));
+    this.goalSelector.a(5, new PathfinderGoalMoveTowardsRestriction(this, 1.0D));
+    this.goalSelector.a(6, new PathfinderGoalMoveThroughVillage(this, 1.0D, false));
+    this.goalSelector.a(7, new PathfinderGoalRandomStroll(this, 1.0D));
+    this.goalSelector.a(8, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F));
+    this.goalSelector.a(8, new PathfinderGoalRandomLookaround(this));
+    this.targetSelector.a(1, new PathfinderGoalHurtByTarget(this, true));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget(this, EntityVillager.class, true, true));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget(this, EntityHuman.class, true, false));
+  }
+}
